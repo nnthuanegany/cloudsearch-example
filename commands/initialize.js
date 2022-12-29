1d251f26d8dac31e095a62815c6e462206e7525a
nnthuanegany <thuan.nguyen@egany.com>
2022-12-29 16:02:56 +0700

    Remove not used data

--- commands/initialize.js
+++ commands/initialize.js
@@ -1,6 +1,5 @@
 const { initialize } = require('../main')
 const egaStyleStore = require('../store/ega-style.json')
-const productDocuments = require('../data/product-documents-2022-12-28T03:19:10.545Z.json')
 const collectionIndexFields = require('../index-fields/collection.json')
 const productIndexFields = require('../index-fields/product.json')
 
