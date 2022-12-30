ddeef44a56ebe15f70e2b476656ae0c15f46f3c9
nnthuanegany <thuan.nguyen@egany.com>
2022-12-30 10:23:06 +0700

    Update new cases for search proudcts

--- commands/products/search.js
+++ commands/products/search.js
@@ -1,5 +1,6 @@
 const { createClient, search } = require('../../client')
 const products = require('../../data/product-documents-2022-12-29T09:00:05.438Z.json')
+const collections = require('../../data/collection-documents-2022-12-28T03:12:48.077Z.json')
 const { toNonAccentVietnamese } = require('../../util')
 
 // The endpoint you will store during initialization domain search
@@ -8,11 +9,22 @@ const client = createClient({ endpoint })
 
 async function main() {
 
-  await searchWithExactName()
-  console.log('-------------------')
-  await searchWithNonAccentVietnameseName()
-  console.log('-------------------')
-  await searchSortByDescMinPrice()
+  // await searchWithExactName()
+  // console.log('-------------------')
+  // await searchWithNonAccentVietnameseName()
+  // console.log('-------------------')
+  // await searchSortByDescMinPrice()
+  // console.log('-------------------')
+  // await searchProductsOfCollection()
+  // console.log('-------------------')
+  // await searchAllProducts()
+  // console.log('-------------------')
+  // await searchAllProductsWitPagination()
+  // console.log('-------------------')
+  // await searchProductsOfCollectionAndFilterColor()
+  // console.log('-------------------')
+  await searchProductsOfCollectionAndFilterRangePrice()
+
 }
 
 async function searchWithExactName() {
@@ -52,6 +64,70 @@ async function searchSortByDescMinPrice() {
     result.hits.hit[0].fields.min_price[0] < result.hits.hit[1].fields.min_price[0] ||
     result.hits.hit[1].fields.min_price[0] < result.hits.hit[2].fields.min_price[0])
     console.warn('==> [FAILED] Min price must be desc')
+  else console.log('==> PASSED')
+}
+
+async function searchProductsOfCollection() {
+  console.log('Search products of a collection')
+  /**@type {import('aws-sdk').CloudSearchDomain.SearchRequest} */
+  const params = { query: `(phrase field=collection_slug '${collections[0].fields.slug}')`, sort: 'min_price desc', queryParser: 'structured' }
+  console.log('Params: ', params)
+  const result = await search({ client, params })
+  console.log('==> Times: %d ms', result.status.timems)
+  // console.log(JSON.stringify(result.hits, null, 4))
+  if (result.hits.found !== collections[0].fields.products_count) console.warn('==> [FAILED] Must be found %d but foud %d', collections[0].fields.products_count, result.hits.found)
+  else console.log('==> PASSED')
+}
+
+async function searchAllProducts() {
+  console.log('Search all products')
+  let total = 0
+  for (const collection of collections) total += collection.fields.products_count
+  /**@type {import('aws-sdk').CloudSearchDomain.SearchRequest} */
+  const params = { query: 'matchall', sort: 'min_price desc', queryParser: 'structured' }
+  console.log('Params: ', params)
+  const result = await search({ client, params })
+  console.log('==> Times: %d ms', result.status.timems)
+  // console.log(JSON.stringify(result.hits, null, 4))
+  if (result.hits.found !== total) console.warn('==> [FAILED] Must be found %d but foud %d', total, result.hits.found)
+  else console.log('==> PASSED')
+}
+
+async function searchAllProductsWitPagination() {
+  console.log('Search all products with pagination')
+  let total = 0
+  for (const collection of collections) total += collection.fields.products_count
+  /**@type {import('aws-sdk').CloudSearchDomain.SearchRequest} */
+  const params = { query: 'matchall', sort: 'min_price desc', queryParser: 'structured', size: 2, start: 2 }
+  console.log('Params: ', params)
+  const result = await search({ client, params })
+  console.log('==> Times: %d ms', result.status.timems)
+
+  if (result.hits.found !== total) console.warn('==> [FAILED] Must be total found %d but total foud %d', total, result.hits.found)
+  else if (result.hits.start !== 2) console.warn('==> [FAILED] Must be start %d but start %d', 2, result.hits.start)
+  else if (result.hits.hit.length !== 2) console.warn('==> [FAILED] Must be return %d but return %d', 2, result.hits.hit.length)
+  else console.log('==> PASSED')
+}
+
+async function searchProductsOfCollectionAndFilterColor() {
+  console.log('Search products of a collection and filter color')
+  /**@type {import('aws-sdk').CloudSearchDomain.SearchRequest} */
+  const params = { query: `(and (phrase field=collection_slug 'vest-blazer') (phrase field=options_values 'Cam'))`, sort: 'min_price desc', queryParser: 'structured' }
+  console.log('Params: ', params)
+  const result = await search({ client, params })
+  console.log('==> Times: %d ms', result.status.timems)
+  if (result.hits.found !== 1) console.warn('==> [FAILED] Must be found %d but foud %d', 1, result.hits.found)
+  else console.log('==> PASSED')
+}
+
+async function searchProductsOfCollectionAndFilterRangePrice() {
+  console.log('Search products of a collection and filter range price')
+  /**@type {import('aws-sdk').CloudSearchDomain.SearchRequest} */
+  const params = { query: `(and (phrase field=collection_slug 'vest-blazer') (range field=min_price [2000000,3000000]))`, sort: 'min_price desc', queryParser: 'structured' }
+  console.log('Params: ', params)
+  const result = await search({ client, params })
+  console.log('==> Times: %d ms', result.status.timems)
+  if (result.hits.found !== 2) console.warn('==> [FAILED] Must be found %d but foud %d', 2, result.hits.found)
   else console.log('==> PASSED')
 }
 
